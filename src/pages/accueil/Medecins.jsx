3ac0c27026e169f99081bc29443289c256b11e6a
Asmae Zaloufi <asmae.zaloufi.pro@gmail.com>
2025-03-13 08:57:22 +0100

    v15:

--- src/pages/accueil/Medecins.jsx
+++ src/pages/accueil/Medecins.jsx
@@ -2,93 +2,151 @@ import { useNavigate, useLocation, useOutletContext } from 'react-router-dom'
 //import reactLogo from '../assets/react.svg'
 import { useState, useEffect } from 'react';
 import './Medecins.css';
-import { getMedecins } from '../../api/medcinapi.jsx';
 import Accueil from './Accueil.jsx';
+import api from '../../api/api.jsx';
+
 
 function Medecins(){
-   const { state } = useLocation(); 
-   const [medecin, setMedecin] = useState(state ? state.user : null);
-   const navigate = useNavigate(); //pour utiliser la navigation du routeur 
-   const [listeVisible, setListeVisible] = useState(false); // Etat visibilité de la liste
-   const [nomMedecin, setNomMedecin]= useState(''); //Etat champ de saisie 
-  // const .{nom , prenom } = medecin;
-   const [listeMedecins, setListeMedecins] = useState([]); //liste qui va contenir les medecins trouvés 
-   const [ version, setVersion] = useState(0); //etat qui permet de forcer le rafraichissement du contenu
-   console.log(state);
-  function charger(e) {
-   const valeursaisie = e.target.value;
-   setNomMedecin(valeursaisie);
-   if (medecin?.nom === valeursaisie) {
-      return medecin.nom + medecin.prenom; 
-   }
-   /*
-       if(nomMedecin != null){
-           if(medecin.nom== nomMedecin){
-              return medecin.nom + medecin.prenom; 
-            }*/
-   }
-  
-  /**
-  * Fonction qui va se declencher lorsqu'un medecin est selectionné dans la liste
-  * @param {JSON} leMedecin // utilisée dans les commentaires JSDoc pour documenter une fonction JavaScript
-  */
-   
-   
-  function selectMedecin(leMedecin){
-     console.log(leMedecin.nom);
-     setMedecin (leMedecin);
-     setNomMedecin (leMedecin.nom + leMedecin.prenom);
-     if(medecin){ // ou medecin ===true  ; === compare les valeurs et les types
-       setListeVisible(false); 
+  const { state } = useLocation();
+  //const [medecin, setMedecin] = useState(state ? state.user : null);
+  const [medecins, setMedecins] = useState([]);
+  const navigate = useNavigate(); //pour utiliser la navigation du routeur
+  const [listeVisible, setListeVisible] = useState(false); // Etat visibilité de la liste
+  const [nomMedecin, setNomMedecin]= useState(''); //Etat champ de saisie
+ // const .{nom , prenom } = medecin;
+  const [listeMedecins, setListeMedecins] = useState([]); //liste qui va contenir les medecins trouvés
+  const [ version, setVersion] = useState(0); //etat qui permet de forcer le rafraichissement du contenu
+ 
+/* function charger(e) {
+  const valeursaisie = e.target.value;
+  setNomMedecin(valeursaisie);
+  if (medecin?.nom === valeursaisie) {
+     return medecin.nom + medecin.prenom;
+  }*/
+  /*
+      if(nomMedecin != null){
+          if(medecin.nom== nomMedecin){
+             return medecin.nom + medecin.prenom;
+           }*/
+  async function charger(e){
+     const valeursaisie = e.target.value;   // Récupérer la saisie de l'utilisateur
+     setNomMedecin(valeursaisie);
+     try{
+        console.log("Recherche en cours pour :", valeursaisie); 
+        const medecinsTrouves = await api.get('/medecins?nom='+valeursaisie); //// Appel à l'API pour récupérer les médecins
+        console.log("Données reçues de l'API :", medecinsTrouves.data);
+        setListeMedecins(medecinsTrouves.data);
+        setListeVisible(true);
+       
      }
-     return leMedecin;
+     catch(error){
+        console.err("Erreur");
+     }
+
+
   }
+ 
+  /**
+ * Fonction qui va se declencher lorsqu'un medecin est selectionné dans la liste
+ * @param {JSON} leMedecin // utilisée dans les commentaires JSDoc pour documenter une fonction JavaScript
+ */
+ 
+ 
+ function selectMedecin(leMedecin){
+    console.log(leMedecin.nom);
+    setMedecins (leMedecin);
+    setNomMedecin (leMedecin.nom + leMedecin.prenom);
+    if(medecins){ // ou medecin ===true  ; === compare les valeurs et les types
+      setListeVisible(false);
+    }
+    return leMedecin;
+ }
+
 
 /*useEffect(()=> { charger();
-   setVersion((prev) => prev + 1); // Force le rafraîchissement  prev représente la valeur actuelle de version, et on retourne prev + 1 pour l’incrémenter.
+  setVersion((prev) => prev + 1); // Force le rafraîchissement  prev représente la valeur actuelle de version, et on retourne prev + 1 pour l’incrémenter.
 }, [nomMedecin]); // Déclenche le useEffect lorsque nomMedecin change);
 
 
+
+
 }
-    /**
-     * Appel à l'API /medecins, avec comme paramètre 'nom'
-     * retour en fonction de la saisie de l'utilisateur
-     * @returns 
-     */
+   /**
+    * Appel à l'API /medecins, avec comme paramètre 'nom'
+    * retour en fonction de la saisie de l'utilisateur
+    * @returns
+    */
 
 
-   async function RechercherRapports(){
 
 
+  async function RechercherRapports(){
+
+
+
+
+ 
+  }
+  return (
+   <>
+    
+  <div style= {{
+     margin:'10px 50px',
+     width: '50%' ,
+     backgroundColor: '#4bb7de',
+     display: 'inline-block',
+     border: '1px solid #ccc',
+     borderRadius:'4px',
+     boxSizing: 'border-box'
+
+
+  }}>
+      <label> <b>Recherche un medecin</b></label>
+      <br/>
+      <input  type ="text"  value={nomMedecin}  onChange= {charger}  style={{width: '96%' ,
+        padding: '4px 20px',
+        margin: '8px 10px',
+        display: 'inline-block',
+        border: '1px solid #ccc',
+        borderRadius:'4px',
+        boxSizing: 'border-box'
+        }}
+        />
+         {listeVisible &&(
+    <ul style={{ margin:'10px 50px',
+     width: '50%'  , color:'#fff'}}>
+        {listeMedecins.map((medecin) =>(
+        <li key={medecin.id}
+           onClick = { () => selectMedecin(medecin) } >
+           { medecin.nom } {medecin.prenom}
+        </li>
+        ))}
+      </ul>
+     )}
+  </div>
    
-   }
-   return (
-    <>
-      
+   <br/>
+     
 
-       <label> Recherche un medecin</label>
-       <br/>
-       <input  type ="text"  value={nomMedecin}  onChange= {charger} />
-       
-       <ul>
-         {listeVisible && listeMedecins.map((medecin ) =>
-         <li key={medecin.id}
-            onClick = { () => selectMedecin(medecin) } >
-            { medecin.nom } {medecin.prenom}
-         </li>
-         )}
-       </ul>
 
-    <br/>
-       
+   </>
+  )
 
-    </>
-   )
+
 
 
 }
+
 
 export default Medecins;
 
+
 //        <input  type ="text"  value={(nomMedecin)}  onChange= {charger} />
-       
+/*<ul>
+{listeVisible && listeMedecins.map(medecin =>
+<li key={medecin.id}
+  onClick = { () => selectMedecin(medecin) } >
+  { medecin.nom } {medecin.prenom}
+</li>
+)}
+</ul>*/
